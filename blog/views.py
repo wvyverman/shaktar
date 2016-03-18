6b799146b3d1a61cec425492d389f2b978b3f5c6
wvyverman <wvyverman@gmail.com>
2016-03-18 10:42:17 +0100

    Working post display

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+	posts = Post.objects.filter(gepubliceerd_op__lte= timezone.now()).order_by('gepubliceerd_op')
+	return render(request, 'blog/post_list.html', {'posts': posts})
