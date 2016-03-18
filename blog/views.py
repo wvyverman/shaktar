3929f706215b88d1c7820669d4f3c78f58179687
wvyverman <wvyverman@gmail.com>
2016-03-18 13:47:01 +0100

    View and template CSS

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(gepubliceerd_op__lte= timezone.now()).order_by('gepubliceerd_op')
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
